cb265c7a9afd1fdaddf970d58e1af096de5ea294
Daniel Hutchinson <dhw4095@gmail.com>
2018-10-27 12:34:08 -0400

    Handle all caps acronyms, no vowels

--- src/main/java/org/spilth/piglatin/Translator.java
+++ src/main/java/org/spilth/piglatin/Translator.java
@@ -3,6 +3,8 @@ package org.spilth.piglatin;
 import java.util.List;
 import java.util.Locale;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Translator {
 
 	/**
@@ -29,8 +31,20 @@ public class Translator {
 	private String swapAndAddWay(String word) {
 		int firstVowelIndex = WordUtil.getIndexOfFirstVowel(word);
 
+		if(firstVowelIndex == -1) {
+			return word + "ay";
+		}
 		String piglatinStart = word.substring(firstVowelIndex);
-		String piglatinEnd = word.substring(0, firstVowelIndex).toLowerCase(Locale.ENGLISH);
+		
+		boolean allUpper = StringUtils.isAllUpperCase(word);
+		String piglatinEnd = null;
+		if(allUpper) {
+			piglatinEnd = word.substring(0, firstVowelIndex);
+		}
+		else {
+			piglatinEnd = word.substring(0, firstVowelIndex).toLowerCase(Locale.ENGLISH);
+		}
+				
 		String piglatinWord = piglatinStart + piglatinEnd + "ay";
 
 		if (WordUtil.isCapitalized(word)) {
